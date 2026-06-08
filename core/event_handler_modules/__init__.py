4347a9c60207b07974d312b0a3c39e4c322d8a4e
lxfight <1686540385@qq.com>
2026-06-08 23:09:08 +0800

    chore: 创建 event_handler_modules 子模块目录
    准备开始重构 event_handler.py

---
+++ core/event_handler_modules/__init__.py
@@ -0,0 +1,8 @@
+"""
+Event Handler Modules Package
+
+This package contains the modularized components of the EventHandler class.
+Each module handles a specific aspect of event processing.
+"""
+
+# Exports will be added as we create modules
